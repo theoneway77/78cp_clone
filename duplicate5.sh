21b2212c28b3e0eb0b3eaa17e6685dea8ecbeff0
theoneway77 <theoneway77@gmail.com>
2014-06-17 20:30:43 +0000

    updating duplicate5.sh,
     adding "${11}" for a fifth output file.

--- duplicate5.sh
+++ duplicate5.sh
@@ -4,10 +4,13 @@ dcfldd if=~/78cp_clone/source.txt | tee >(dd of=~/78cp_clone/un.txt) >(dd of=~/7
 set `sha256sum -b source.txt un.txt deux.txt trois.txt quatre.txt cinque.txt`
 echo $1 
 echo $3
+echo $5
+echo $7
 echo $9
+echo ${11}
 sleep 6
 
-  if [ $1 = $3 -a $3 = $5 -a $5 = $7 -a $7 = $9 ]
+  if [ $1 = $3 -a $3 = $5 -a $5 = $7 -a $7 = $9 -a $9 = ${11} ]
 	then echo "Duplication Successful"
 	else echo "There was an error while copying."
   fi
