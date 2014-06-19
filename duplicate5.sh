4b80d768855830bb7990e51b5a8c6a421b4dd341
theoneway77 <theoneway77@gmail.com>
2014-06-19 21:03:19 +0000

    Added a comparison test based on positional parameters from err.txt.  Added some comments and cleaned up code.

--- duplicate5.sh
+++ duplicate5.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
+#copy source to -n destinations
 dcfldd if=~/78cp_clone/source.txt 2>>err.txt | tee >(dd of=~/78cp_clone/un.txt 2>>err.txt) >(dd of=~/78cp_clone/deux.txt 2>>err.txt ) >(dd of=~/78cp_clone/trois.txt 2>>err.txt) >(dd of=~/78cp_clone/quatre.txt 2>>err.txt) | dd of=~/78cp_clone/cinque.txt 2>>err.txt
 
+#Assign positional parameter variables to all words in any line with the word `copied` in it from the file err.txt.  
+set `grep -iw "copied" err.txt`
+#If the first word in the line (which is the number of bytes copied) is the same in all -n lines, then the duplication was successful.  
+#Otherwise it may not have been.  
+if [ $1 -eq ${10} -a ${10} = ${19} -a ${19} = ${28} -a ${28} = ${37} ]
+then 
+	echo "Duplication Great Success!"
+else
+	echo "there may have been a problem with the transfer. Please check your media."
+fi
+#cut and paste the contents of err.txt into errlog.txt.  This will give us a fresh err.txt to evaluate for the next iteration. 
+cat err.txt >> errlog.txt
+rm err.txt
 
-##compare source file to the french duplicates using  sha256sums for source.txt, un.txt, deux.txt, trois.txt, quatre.txt, and cinque.txt. 
-#set `sha256sum -b source.txt un.txt deux.txt trois.txt quatre.txt cinque.txt`
-#echo $1 
-#echo $3
-#echo $5
-#echo $7
-#echo $9
-#echo ${11}
-#sleep 6
-
-#  if [ $1 = $3 -a $3 = $5 -a $5 = $7 -a $7 = $9 -a $9 = ${11} ]
-#	then echo "Duplication Successful"
-#	else echo "There was an error while copying."
- # fi
