d05493099bbb4b6ed9f38cf432977fb2d7903583
theoneway77 <theoneway77@gmail.com>
2014-06-19 19:39:20 +0000

    1 mod and 7 new files.
    Modified Duplicate5.sh to redirect standard error to err.txt.
    Added source.txt, err.txt, un.txt, deux.txt, trois.txt, quatre.txt, cinque.txt

--- duplicate5.sh
+++ duplicate5.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-dcfldd if=~/78cp_clone/source.txt | tee >(dd of=~/78cp_clone/un.txt) >(dd of=~/78cp_clone/deux.txt) >(dd of=~/78cp_clone/trois.txt) >(dd of=~/78cp_clone/quatre.txt) | dd of=~/78cp_clone/cinque.txt
-#compare source file to the french duplicates using  sha256sums for source.txt, un.txt, deux.txt, trois.txt, quatre.txt, and cinque.txt. 
-set `sha256sum -b source.txt un.txt deux.txt trois.txt quatre.txt cinque.txt`
-echo $1 
-echo $3
-echo $5
-echo $7
-echo $9
-echo ${11}
-sleep 6
 
-  if [ $1 = $3 -a $3 = $5 -a $5 = $7 -a $7 = $9 -a $9 = ${11} ]
-	then echo "Duplication Successful"
-	else echo "There was an error while copying."
-  fi
+dcfldd if=~/78cp_clone/source.txt 2>>err.txt | tee >(dd of=~/78cp_clone/un.txt 2>>err.txt) >(dd of=~/78cp_clone/deux.txt 2>>err.txt ) >(dd of=~/78cp_clone/trois.txt 2>>err.txt) >(dd of=~/78cp_clone/quatre.txt 2>>err.txt) | dd of=~/78cp_clone/cinque.txt 2>>err.txt
+
+
+##compare source file to the french duplicates using  sha256sums for source.txt, un.txt, deux.txt, trois.txt, quatre.txt, and cinque.txt. 
+#set `sha256sum -b source.txt un.txt deux.txt trois.txt quatre.txt cinque.txt`
+#echo $1 
+#echo $3
+#echo $5
+#echo $7
+#echo $9
+#echo ${11}
+#sleep 6
+
+#  if [ $1 = $3 -a $3 = $5 -a $5 = $7 -a $7 = $9 -a $9 = ${11} ]
+#	then echo "Duplication Successful"
+#	else echo "There was an error while copying."
+ # fi
