c1bd77efe7618538245ef208a265f517a001d47b
theoneway77 <theoneway77@gmail.com>
2014-06-20 21:02:47 +0000

    Updating duplicate5.sh
    Made syntax & file more readable.

--- duplicate5.sh
+++ duplicate5.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-#copy source to -n destinations
+#copy source to -n destinations and redirect error to err.txt
 dcfldd if=~/78cp_clone/source.txt 2>>err.txt | tee >(dd of=~/78cp_clone/un.txt 2>>err.txt) >(dd of=~/78cp_clone/deux.txt 2>>err.txt ) >(dd of=~/78cp_clone/trois.txt 2>>err.txt) >(dd of=~/78cp_clone/quatre.txt 2>>err.txt) | dd of=~/78cp_clone/cinque.txt 2>>err.txt
+
+#2 carriage returns for errlog.txt to manke it more readable. 
+echo -e "\r\r" >> errlog.txt
 
 #Append the date and time to errlog.txt
 date >> errlog.txt 
+
 #Assign positional parameter variables to all words in any line with the word `copied` in it from the file err.txt.  
 set `grep -iw "copied" err.txt`
+
 #If the first word in the line (which is the number of bytes copied) is the same in all -n lines, then the duplication was successful.  
 #Otherwise it may not have been.  
 if [ $1 -eq ${10} -a ${10} = ${19} -a ${19} = ${28} -a ${28} = ${37} ]
@@ -15,9 +20,13 @@ then
 else
 	echo "there may have been a problem with the transfer. Please check your media."
 fi
-#cut and paste the contents of err.txt into errlog.txt.  This will give us a fresh err.txt to evaluate for the next iteration. 
+
+#Paste the contents of err.txt into errlog.txt. 
 cat err.txt >> errlog.txt
+
 #2 carriage returns for errlog.txt to manke it more readable. 
 echo -e "\r\r" >> errlog.txt
+
+#delete err.txt This will give us a fresh err.txt to evaluate for the next iteration. 
 rm err.txt
 
