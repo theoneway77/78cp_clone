82a5e257fa7404f2bc5b4c4570f026bf7618080a
theoneway77 <theoneway77@gmail.com>
2014-06-22 05:37:06 +0000

    updating duplicate5
    adding if statement to check if for integer value.

--- duplicate5.sh
+++ duplicate5.sh
@@ -12,13 +12,17 @@ date >> errlog.txt
 #Assign positional parameter variables to all words in any line with the word `copied` in it from the file err.txt.  
 set `grep -iw "copied" err.txt`
 
-#If the first word in the line (which is the number of bytes copied) is the same in all -n lines, then the duplication was successful.  
-#Otherwise it may not have been.  
-if [ $1 -eq ${10} -a ${10} = ${19} -a ${19} = ${28} -a ${28} = ${37} ]
-then 
-	echo "Duplication Great Success!"
-else
-	echo "there may have been a problem with the transfer. Please check your media."
+#Check if $1 is a number. If so, then check if all file tranfer sizes are the same.  
+if [[ $1 != [0-9]* ]]
+  then
+  echo "variable 1 is not a number!"
+  else
+	if [ $1 -eq ${10} -a ${10} = ${19} -a ${19} = ${28} -a ${28} = ${37} ]
+	then 
+		echo "Duplication Great Success!"
+	else
+		echo "there may have been a problem with the transfer. Please check your media."
+	fi
 fi
 
 #Paste the contents of err.txt into errlog.txt. 
@@ -27,6 +31,6 @@ cat err.txt >> errlog.txt
 #2 carriage returns for errlog.txt to manke it more readable. 
 echo -e "\r\r" >> errlog.txt
 
-#delete err.txt This will give us a fresh err.txt to evaluate for the next iteration. 
+#delete err.txt This wnill give us a fresh err.txt to evaluate for the next iteration. 
 rm err.txt
 
